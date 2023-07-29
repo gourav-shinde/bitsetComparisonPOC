423c82ae95c8af7caa6219c7b9c3c440f3241211
gourav-shinde <gauravsanjayshinde@gmail.com>
2023-07-29 15:31:19 -0400

    scaling

--- main.cpp
+++ main.cpp
@@ -5,15 +5,23 @@
 #include"bitsetPOC.cpp"
 
 
-std::int32_t main(){
+std::int32_t main(int argc, char** argv){
+    int numIterations = atoi(argv[1]);
+
     RandomGenerator randomGenerator;
-    std::uint64_t sendTime = randomGenerator.generateRandomLongLongInt();
-    std::uint64_t receiveTime = randomGenerator.generateRandomLongLongInt();
-    std::string sendName = randomGenerator.generateRandomString();
-    std::string receiveName = randomGenerator.generateRandomString();
-    std::uint8_t generation = randomGenerator.generateRandomUint8();
+    std::uint64_t sendTime ,receiveTime;
+    std::string sendName, receiveName;
+    std::uint8_t generation;
+    while(numIterations--){
+    sendTime = randomGenerator.generateRandomLongLongInt();
+    receiveTime = randomGenerator.generateRandomLongLongInt();
+    sendName = randomGenerator.generateRandomString();
+    receiveName = randomGenerator.generateRandomString();
+    generation = randomGenerator.generateRandomUint8();
+
     Event event1(sendTime, receiveTime, sendName, receiveName, generation);
     Event2 eventA(sendTime, receiveTime, sendName, receiveName, generation);
+
     sendTime = randomGenerator.generateRandomLongLongInt();
     receiveTime = randomGenerator.generateRandomLongLongInt();
     sendName = randomGenerator.generateRandomString();
@@ -22,7 +30,14 @@ std::int32_t main(){
 
     Event event2(sendTime, receiveTime, sendName, receiveName, generation);
     Event2 eventB(sendTime, receiveTime, sendName, receiveName, generation);
+    
 
-    std::cout<<compareEvents(event1, event2)<<std::endl;
-    std::cout<<compareEvents2(eventA, eventB)<<std::endl;
+    if(compareEvents(event1, event2) != compareEvents2(eventA, eventB)){
+        std::cout<<"False"<<std::endl;
+        break;
+    }
+    
+
+    }
+
 }
