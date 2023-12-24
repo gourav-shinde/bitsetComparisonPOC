aabfe7a7e1cbe08c1e215cfb592ae51028977292
gourav-shinde <gauravsanjayshinde@gmail.com>
2023-12-23 20:29:57 -0500

    -ve indexing prototype stable

--- UnifiedQueue.hpp
+++ UnifiedQueue.hpp
@@ -26,9 +26,9 @@ public:
         queue_.resize(capacity);
         // change free start to 0
         freeStart_.store(0, std::memory_order_relaxed); // memory order relaxed, same for all 3
-        // change active and unprocessed to -1
-        activeStart_.store(-1, std::memory_order_relaxed);
-        unprocessedStart_.store(-1, std::memory_order_relaxed);
+        // change active and unprocessed to INT32_MIN
+        activeStart_.store(INT32_MIN, std::memory_order_relaxed);
+        unprocessedStart_.store(INT32_MIN, std::memory_order_relaxed);
     }
     //adding some comment to test git action
     // check logic
@@ -38,15 +38,15 @@ public:
 
     // checks if freeStart_ is one index behind activeStart_ zone, if yes then queue is full
     bool isFull() {
-        
-        if (freeStart_.load(std::memory_order_relaxed) == activeStart_.load(std::memory_order_relaxed)) {
+        //when freeStart_ is -ve, queue is full
+        if (freeStart_.load(std::memory_order_relaxed) < 0) {
             return true;
         }
         return false;
     }
 
     // returns queue size
-    uint64_t capacity(){
+    uint64_t capacity() {
         return queue_.size();
     }
 
@@ -55,37 +55,46 @@ public:
         uint64_t queue_size = 0; // change name
 
         // Initial case
-        if (activeStart_.load(std::memory_order_relaxed) == -1) {
+        if ( activeStart_.load( std::memory_order_relaxed) == INT32_MIN ) {
             return queue_size;
         }
+        else if( freeStart_.load(std::memory_order_relaxed) < 0 ){
+            return queue_.size(); //when queue is full
+        }
         // when there is no rotation in queue
-        if (freeStart_.load(std::memory_order_relaxed) > activeStart_.load(std::memory_order_relaxed)) {
-            queue_size = freeStart_.load(std::memory_order_relaxed) - activeStart_.load(std::memory_order_relaxed);
+        else if (getValidIndex(freeStart_.load(std::memory_order_relaxed)) > getValidIndex(activeStart_.load(std::memory_order_relaxed))) {
+            queue_size = getValidIndex(freeStart_.load(std::memory_order_relaxed)) - getValidIndex(activeStart_.load(std::memory_order_relaxed));
         }
         // rotation i.e fossileStart_ < activeStart_
-        else if (freeStart_.load(std::memory_order_relaxed) < activeStart_.load(std::memory_order_relaxed)) {
-            queue_size = capacity() - activeStart_.load(std::memory_order_relaxed) + freeStart_.load(std::memory_order_relaxed);
+        else if (getValidIndex(freeStart_.load(std::memory_order_relaxed)) < getValidIndex(activeStart_.load(std::memory_order_relaxed))) {
+            queue_size = capacity() - getValidIndex(activeStart_.load(std::memory_order_relaxed)) + getValidIndex(freeStart_.load(std::memory_order_relaxed));
         }
-        else if(activeStart_.load(std::memory_order_relaxed) == freeStart_.load(std::memory_order_relaxed)){
+        else if (activeStart_.load(std::memory_order_relaxed) == freeStart_.load(std::memory_order_relaxed)) {
             return 0;//queue is empty , this is when freeStart_ == activeStart_ == unprocessedStart_
         }
-        else{ // when freeStart_ == activeStart_ (queue is full)
+        else { // when freeStart_ == activeStart_ (queue is full)
             return queue_.size();
         }
         return queue_size;
+
+        //
     }
 
     // Print the current state of queue
     void debug() {
         std::cout << "activeStart_: " << activeStart_.load(std::memory_order_relaxed) << " unprocessedStart_: " << unprocessedStart_.load(std::memory_order_relaxed) << " freeStart_: " << freeStart_.load(std::memory_order_relaxed) << " size: " << this->size() << std::endl;
 
-        int i = unprocessedStart_.load(std::memory_order_relaxed);
-        do{
+        int i = (unprocessedStart_.load(std::memory_order_relaxed)<0)?-1:unprocessedStart_.load(std::memory_order_relaxed);
+        if(i!=-1){
+        do {
             std::cout << queue_[i].receiveTime_ << " ";
             i = nextIndex(i);
-        }while(i!=freeStart_.load(std::memory_order_relaxed));
+        } while (i != getValidIndex(abs(freeStart_.load(std::memory_order_relaxed))));
         std::cout << std::endl;
-        
+        }
+        else{
+            std::cout<<"Unprocessed Events Empty"<<std::endl;
+        }
         for (auto itr : queue_) {
             std::cout << itr.receiveTime_ << " ";
         }
@@ -157,16 +166,27 @@ public:
         }
     }
 
+    //modulo for index
+    int getValidIndex(int index) {
+        return abs(index) % capacity();
+    }
+
     // enqueue the value
     void enqueue(T value) {
         // increment freeStart_ index first for ABA. 
         // store freeStart_ index in a variable and use it in the function and then pass it next
         // this changes
+
+        //preprocess freeStart
+        //check if freeStart is -ve  or not.
+        //this includes doing modulo operation and checking for is queue full or not.
+
         if (isFull()) {
             std::cout << "Queue is full, can't insert element." << std::endl;
             return;
         }
-
+        freeStart_.store(freeStart_.load(std::memory_order_relaxed) % capacity(), std::memory_order_relaxed);
+        unprocessedStart_.store(getValidIndex(unprocessedStart_.load(std::memory_order_relaxed)), std::memory_order_relaxed);
         // checking for rollback
         if (isEmpty()) {
             // make unprocessed and active to 0
@@ -179,21 +199,40 @@ public:
             shiftElements(insertPos, freeStart_.load(std::memory_order_relaxed));
             queue_[insertPos] = value;
         }
-        freeStart_.store(nextIndex(freeStart_.load(std::memory_order_relaxed)), std::memory_order_relaxed);
+        //check if freeStart+1 == activeStart, if then freeStart = -(freeStart++), else freeStart++
+        //do modulo on freeStart when doing the insert.
+        if (nextIndex(freeStart_.load(std::memory_order_relaxed)) == activeStart_.load(std::memory_order_relaxed)) {
+            freeStart_.store(-(freeStart_.load(std::memory_order_relaxed)+1), std::memory_order_relaxed);
+        }
+        else {
+            freeStart_.store((freeStart_.load(std::memory_order_relaxed)+1), std::memory_order_relaxed);
+        }
+        //not using nextIndex() here as we want to do module at start of the function 
     }
 
     // dequeue returns start of unprocessed. and increment it
     T dequeue() {
-        if (isEmpty()) {
+        if (isEmpty()) { //when whole queue is empty
             std::cout << "Queue is empty, can't remove element." << std::endl;
             return T();
         }
-        if (unprocessedStart_.load(std::memory_order_relaxed) == freeStart_.load(std::memory_order_relaxed)) {
+
+        if (unprocessedStart_.load(std::memory_order_relaxed) < 0) {
             std::cout << "Unprocessed Events Empty" << std::endl;
             return T();
         }
-        
+        //when activeStart_ is -ve, it means activeStart_ is empty, so with dequeue we are adding a element to it
+        activeStart_.store(getValidIndex(activeStart_.load(std::memory_order_relaxed)), std::memory_order_relaxed);
+
         T value = queue_[unprocessedStart_.load(std::memory_order_relaxed)];
+        
+        if(nextIndex(unprocessedStart_.load(std::memory_order_relaxed)) == getValidIndex(freeStart_.load(std::memory_order_relaxed))){
+          //this means unprocessed list is empty
+            unprocessedStart_.store(-(unprocessedStart_.load(std::memory_order_relaxed)+1), std::memory_order_relaxed);
+            return value;
+        }
+
+        
         unprocessedStart_.store(nextIndex(unprocessedStart_.load(std::memory_order_relaxed)), std::memory_order_relaxed);
         return value;
     }
@@ -237,9 +276,28 @@ public:
 
     // increament freeStart_ index
     int increamentActiveStart_Index() {
-        if (nextIndex(activeStart_.load(std::memory_order_relaxed)) > unprocessedStart_.load(std::memory_order_relaxed)) {
-            std::cout << "Cannot do that. activeStart goes in front of unprocessed" << std::endl;
+        //check if freeStart -ve, update with abs(freeStart), module operation too
+        if(activeStart_.load(std::memory_order_relaxed) == INT32_MIN){
+            std::cout << "Cannot do that. activeStart it is inital condition" << std::endl;
             return -1;
+        }
+        else if(isEmpty()){
+            std::cout << "Cannot do that. Whole queue is empty" << std::endl;
+            return -1;
+        }
+        else if (freeStart_.load(std::memory_order_relaxed) < 0) {
+            freeStart_.store(getValidIndex(abs(freeStart_.load(std::memory_order_relaxed))), std::memory_order_relaxed);
+        }
+        else if(activeStart_.load(std::memory_order_relaxed) < 0){
+            std::cout << "Cannot do that. activeStart is -ve means its empty" << std::endl;
+            return -1;
+        }
+        else if ((nextIndex(activeStart_.load(std::memory_order_relaxed))) == unprocessedStart_.load(std::memory_order_relaxed)) {
+            activeStart_.store(-(activeStart_.load(std::memory_order_relaxed)+1), std::memory_order_relaxed);
+            return -1;
+        }
+        else{
+
         }
         activeStart_.store(nextIndex(activeStart_.load(std::memory_order_relaxed)), std::memory_order_relaxed);
         return activeStart_.load(std::memory_order_relaxed);
@@ -247,5 +305,5 @@ public:
 
     //increament active start upto a certain timestamp
 
-    
+
 };
--- test_queue.cpp
+++ test_queue.cpp
@@ -98,6 +98,45 @@ TEST(UnifiedQueueTest, PositionTest){
     EXPECT_EQ(pos, 0);
 }
 
+//not on zero
+TEST(UnifiedQueueTest, SizeTest){
+    UnifiedQueue<Event, compareEvent> queue(4);
+    queue.enqueue(Event(1, 1, "a", "b", 1, true));
+    queue.enqueue(Event(2, 2, "a", "b", 1, true));
+    queue.enqueue(Event(3, 3, "a", "b", 1, true));
+    queue.enqueue(Event(4, 4, "a", "b", 1, true));
+    queue.debug();
+    EXPECT_EQ(queue.size(), 4); //00-4  markers
+    queue.dequeue();
+    queue.increamentActiveStart_Index();
+    EXPECT_EQ(queue.size(), 3); //110  markers
+    queue.debug();
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    queue.debug();
+    EXPECT_EQ(queue.size(), 3); //1-40 markers
+    queue.enqueue(Event(5, 5, "a", "b", 1, true)); //10-1 markers
+    queue.debug();
+    queue.increamentActiveStart_Index();
+    queue.debug();
+    queue.increamentActiveStart_Index();
+    queue.increamentActiveStart_Index();
+    queue.debug(); //-401 markers
+    queue.dequeue();
+    queue.debug();
+    queue.increamentActiveStart_Index();
+    queue.debug();
+    EXPECT_EQ(queue.size(), 0); //0-40 markers
+    EXPECT_EQ(queue.isEmpty(), true);
+    queue.increamentActiveStart_Index();
+    queue.debug();
+    queue.dequeue();
+    queue.debug();
+
+    
+}
+
 // test for increamentActiveStart_Index
 
 int main(){
@@ -109,3 +148,35 @@ int main(){
 //discuss about the case where
 //freeStart_ == activeStart_ == unprocessedStart_ (queue is empty)
 //freeStart_ == activeStart_ == unprocessedStart_ (if we do not process any events)(queue is full)
+
+// size 8
+
+// 444 _Empty
+// 445
+// 446
+// 447
+// 440
+// 441
+// 442
+// 453
+// 46-4 _Full   
+// 47-4
+// 40-4
+// 41-4
+// 42-4
+// 43-4
+// 44-4 _Full
+
+
+
+// now
+// in  enqueue.
+// if abs(free)!= active queue is not full and freeStart=abs freeStart.
+
+
+//unprocessed changes
+//freeStart_ == activeStart_ != unprocessedStart_ (certain)
+
+
+// if unprocessed start becomes == freestart then -ve it, to show it is empty.
+// if active start becomes == unprocessed stat then -ve it to show it is empty
