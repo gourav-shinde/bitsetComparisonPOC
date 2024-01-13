2d0872cdcb9b9448a14f7d3ba16defe2f9667413
gourav-shinde <gauravsanjayshinde@gmail.com>
2024-01-12 23:32:35 -0500

    test for find location removed

--- test_queue.cpp
+++ test_queue.cpp
@@ -112,42 +112,42 @@ TEST(UnifiedQueueTest, enqueueTest) {
 
 
 // test for findInsertPosition
-TEST(UnifiedQueueTest, PositionTest){
-    UnifiedQueue<Event, compareEvent> queue(10);
-    int pos = queue.findInsertPosition(Event (1, 1, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
-    EXPECT_EQ(pos, 0);
-    queue.enqueue(Event(1, 1, "a", "b", 1, true));
-    pos = queue.findInsertPosition(Event(3, 3, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
-    EXPECT_EQ(pos, 1);
-    queue.enqueue(Event(3, 3, "a", "b", 1, true));
-    pos = queue.findInsertPosition(Event(2, 2, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
-    EXPECT_EQ(pos, 1);
-    queue.enqueue(Event(2, 2, "a", "b", 1, true)); //let this be here if u wanna add more cases
-    // add rotation cases
-    queue.enqueue(Event(4, 4, "a", "b", 1, true));
-    queue.enqueue(Event(5, 5, "a", "b", 1, true));
-    queue.enqueue(Event(6, 6, "a", "b", 1, true));
-    queue.enqueue(Event(7, 7, "a", "b", 1, true));
+// TEST(UnifiedQueueTest, PositionTest){
+//     UnifiedQueue<Event, compareEvent> queue(10);
+//     int pos = queue.findInsertPosition(Event (1, 1, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
+//     EXPECT_EQ(pos, 0);
+//     queue.enqueue(Event(1, 1, "a", "b", 1, true));
+//     pos = queue.findInsertPosition(Event(3, 3, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
+//     EXPECT_EQ(pos, 1);
+//     queue.enqueue(Event(3, 3, "a", "b", 1, true));
+//     pos = queue.findInsertPosition(Event(2, 2, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart());
+//     EXPECT_EQ(pos, 1);
+//     queue.enqueue(Event(2, 2, "a", "b", 1, true)); //let this be here if u wanna add more cases
+//     // add rotation cases
+//     queue.enqueue(Event(4, 4, "a", "b", 1, true));
+//     queue.enqueue(Event(5, 5, "a", "b", 1, true));
+//     queue.enqueue(Event(6, 6, "a", "b", 1, true));
+//     queue.enqueue(Event(7, 7, "a", "b", 1, true));
     
-    queue.enqueue(Event(8, 8, "a", "b", 1, true));
-    queue.enqueue(Event(9, 9, "a", "b", 1, true));
-    queue.enqueue(Event(10, 10, "a", "b", 1, true));
-    EXPECT_EQ(queue.isFull(), 1);
-    queue.debug();
+//     queue.enqueue(Event(8, 8, "a", "b", 1, true));
+//     queue.enqueue(Event(9, 9, "a", "b", 1, true));
+//     queue.enqueue(Event(10, 10, "a", "b", 1, true));
+//     EXPECT_EQ(queue.isFull(), 1);
+//     queue.debug();
 
-    queue.dequeue();
-    queue.debug();
-    queue.increamentActiveStart(); ; //these two lines replicate fossil collection and event processing
-    queue.dequeue();
-    queue.debug();
-    queue.increamentActiveStart(); ;
+//     queue.dequeue();
+//     queue.debug();
+//     queue.increamentActiveStart(); ; //these two lines replicate fossil collection and event processing
+//     queue.dequeue();
+//     queue.debug();
+//     queue.increamentActiveStart(); ;
 
-    pos = queue.findInsertPosition(Event(11, 11, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart()); //this should be inserted at pos 0
-    EXPECT_EQ(pos, 0);
-    queue.enqueue(Event(11, 11, "a", "b", 1, true));
-    pos = queue.findInsertPosition(Event(11, 10, "a", "b", 2, true),queue.getActiveStart(),queue.getFreeStart()); //this should be inserted at pos 0
-    EXPECT_EQ(pos, 0);
-}
+//     pos = queue.findInsertPosition(Event(11, 11, "a", "b", 1, true),queue.getActiveStart(),queue.getFreeStart()); //this should be inserted at pos 0
+//     EXPECT_EQ(pos, 0);
+//     queue.enqueue(Event(11, 11, "a", "b", 1, true));
+//     pos = queue.findInsertPosition(Event(11, 10, "a", "b", 2, true),queue.getActiveStart(),queue.getFreeStart()); //this should be inserted at pos 0
+//     EXPECT_EQ(pos, 0);
+// }
 
 //not on zero
 TEST(UnifiedQueueTest, SizeTest){
